29727740d60d05e460dfae1785205cda5cc7c19d
Vitor Oliveira <vitor.oliveira@trustly.com>
2023-05-17 13:34:19 +0100

    updates

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,43 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [now, setNow] = useState(null);
 
+  const data = {
+    notInstance: 0,
+    instanceClosed: 0,
+    instanceNotclosed: 0,
+  }
+
   useEffect(() => {
     setNow(new Date().toISOString());
   }, []);
 
+  const popupTest = (event) => {
+    const instance = window.open("/popup");
+    setInterval(() => {
+      if (!instance) {
+        data.notInstance++
+        return
+      }
+      if (instance.closed) {
+        data.instanceClosed++
+        return
+      }
+      data.instanceNotclosed++
+    }, 1000)
+    setTimeout(() => {
+      alert(JSON.stringify(data))
+    }, 5_000)
+    event.preventDefault()
+  }
+
   return (
     <div>
       <h1>
-        Home - v2<small>{now}</small>
+        Home - v3<small>{now}</small>
       </h1>
       <a href="/login">1 - Login (form submit)</a><br/>
-      <a href="/signin">2 - Sign in (iframe target)</a>
+      <a href="/signin">2 - Sign in (iframe target)</a><br/>
+      <a href="#" onClick={popupTest}>3 - Popup test</a>
     </div>
   );
 }
---
+++ pages/popup/index.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export default function Popup() {
+  const [now, setNow] = useState(null);
+
+  useEffect(() => {
+    setNow(new Date().toISOString());
+  }, []);
+
+  const close = () => {
+    window.close()
+  }
+
+  return (
+    <div>
+      <h1>
+        Popup<small>{now}</small>
+      </h1>
+      <a href="#" onClick={close}>Close</a>
+    </div>
+  );
+}
